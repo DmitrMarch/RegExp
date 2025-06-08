05a912210f9d7f4e728c556d03a940085eeba5f8
Dmitriy Marchenko <mdi500100@gmail.com>
2025-06-08 20:35:11 +0300

    fix split regexp

--- splitText.js
+++ splitText.js
@@ -1,6 +1,6 @@
 function splitText(text) {
 
-    const regex = /(?<sentence>[^.!?]*?[А-ЯA-Z][^.!?]*?[.!?])(?:\s+|$)/gud;
+    const regex = /(?<sentence>[А-ЯA-Z][\s\S]*?(?:[.!?](?=\s+[А-ЯA-Z]|$)))/gud;
 
     return [...text.matchAll(regex)];
 }
